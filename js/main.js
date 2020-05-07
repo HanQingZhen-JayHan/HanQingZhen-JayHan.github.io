f56dfcd195d1404b7f6146bc72da0d88d58ce831
JayHan <690346750@qq.com>
2020-05-06 22:43:01 -0700

    perfect navigation

--- js/main.js
+++ js/main.js
@@ -1,17 +1,11 @@
 $(document).ready(function(){
  $('.header').height($(window).height());
 
- $(".header a").click(function(){
-	$("body,html").animate({
-		scrollTop:$("#" + $(this).data('value')).offset().top
-	},1000)
- 
-})
- $(".navbar a").click(function(){
- 	$("body,html").animate({
- 		scrollTop:$("#" + $(this).data('value')).offset().top
- 	},1000)
+//  $(".navbar a").click(function(){
+//  	$("body,html").animate({
+//  		scrollTop:$("#" + $(this).data('value')).offset().top
+//  	},1000)
   
- })
+//  })
 
 })
