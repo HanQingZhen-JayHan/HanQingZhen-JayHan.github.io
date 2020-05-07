884ccc2cb9c4476ae886725c853f7d4c77650c3b
JayHan <690346750@qq.com>
2020-05-06 21:31:45 -0700

    add links

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
  $('.header').height($(window).height());
 
+ $(".header a").click(function(){
+	$("body,html").animate({
+		scrollTop:$("#" + $(this).data('value')).offset().top
+	},1000)
+ 
+})
  $(".navbar a").click(function(){
  	$("body,html").animate({
  		scrollTop:$("#" + $(this).data('value')).offset().top
